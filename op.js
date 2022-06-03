b19646a666b71e67d8fc5c43e3025806dc379cda
sam0531 <104211055+sam0531@users.noreply.github.com>
2022-06-03 21:36:24 +0530

    Add files via upload

---
+++ op.js
@@ -0,0 +1,92 @@
+// 1. Add two Numbers
+// You are provided with two numbers 
+// Note: You have to complete AddTwoNumbers function. No need to take any input.
+
+
+// const AddTwoNumbers = (a,b) => {
+//     var result = a+b;
+//         return result;
+
+// };
+
+// 2. Find if the conditions are obeyed or not?
+
+
+
+// const Is_Valid = (a,b) => {
+//     if(a<10 && a>b){
+//       return true;
+//     }
+//     else{
+//       return false;
+//     }
+    
+//   };
+  
+  
+
+  
+//   3. Check the conditons
+
+//   const Check = (A, B) => 
+// {
+//     if((A%10==0 && B%10==0) || (A%10==0 || B%10==0)){
+//       return true;
+//     }
+//     else{
+//       return false;
+//     }
+// };
+
+// 4. Find the first digit of a 4 digit number
+
+
+// const First_Digit = (n) => {
+//     return parseInt(n/1000);
+//     };
+    
+    
+
+// 5. Find the last digit of a 4 digit number
+
+// const Last_Digit = (n) => {
+//     return n%10;   
+//  };
+ 
+
+//  6.Find the remainder
+
+ 
+// const Find_the_remainder = (a,b) => {
+//     let result =b%a;
+//     return result;
+// };
+
+// 7. Multipy two Numbers
+
+
+// const Multiply_two_number = (a,b) => {
+//     let result =a*b;
+//     return result;
+// };
+
+// 8. Marks Calculator
+
+
+
+// const Sum = (A, B, C) => 
+// {
+//  let sum= A+B+C;
+//  return sum;
+// };
+
+// const Average = (A, B, C) => 
+// {
+//  let total = A+B+C;
+//  let result = (total*100/300);
+//  return result; 
+// };
+
+
+
+
